b34366cc54077ca6f493959e681ca5b493fa7cc8
Pouria <pnsadeghy@gmail.com>
2025-02-13 20:08:09 +0330

    Fix: Updated Dockerfiles for file permissions

--- .deploy/entrypoint.sh
+++ .deploy/entrypoint.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 set -e
 
-chown -R www-data:www-data /var/www/html
+USER_ID=$(stat -c "%u" /var/www/html)
+GROUP_ID=$(stat -c "%g" /var/www/html)
+
+if [ "$USER_ID" -eq 0 ]; then
+    echo "Updating ownership of /var/www/html to match WSL user..."
+    chown -R $(id -u):$(id -g) /var/www/html
+fi
+
 chmod -R 775 /var/www/html
 
 composer install --no-dev --optimize-autoloader
